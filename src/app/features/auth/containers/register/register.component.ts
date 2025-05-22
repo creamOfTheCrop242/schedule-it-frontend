e8b96db75216fcba29e8da51b9a934a54342cd6c
Donald Hudson <donmuscles97@Donalds-MacBook-Pro.local>
2025-05-22 19:38:57 -0400

    finish auth

--- src/app/features/auth/containers/register/register.component.ts
+++ src/app/features/auth/containers/register/register.component.ts
@@ -1,14 +1,27 @@
 import { Component, inject } from '@angular/core';
 import { InputComponent } from '../../../shared/components/input/input.component';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 import { RegisterRequest } from '../../models/auth.model';
 import { take } from 'rxjs';
+import { ButtonComponent } from '../../../shared/components/button/button.component';
+import { ErrorTextComponent } from '../../../shared/components/error-text/error-text.component';
 
 @Component({
   selector: 'app-register',
-  imports: [InputComponent, RouterModule],
+  imports: [
+    InputComponent,
+    RouterModule,
+    ReactiveFormsModule,
+    ButtonComponent,
+    ErrorTextComponent,
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss',
 })
@@ -29,24 +42,29 @@ export class RegisterComponent {
   };
 
   validateForm() {
+    this.formErrors = {
+      name: null,
+      email: null,
+      password: null,
+      confirmPassword: null,
+    };
+
     if (this.form.invalid) {
+      console.log(this.form);
       if (this.form.controls['name'].errors) {
-        this.formErrors.email = 'Please enter a name';
+        this.formErrors.name = 'Please enter a name';
       }
 
       if (this.form.controls['email'].errors) {
-        this.formErrors.password = 'Please enter a valid email';
+        this.formErrors.email = 'Please enter a valid email';
       }
 
       if (this.form.controls['password'].errors) {
         this.formErrors.password = 'Please enter a valid password';
       }
 
-      if (
-        this.form.controls['password'].value !==
-        this.form.controls['confirmPassword'].value
-      ) {
-        this.formErrors.password = 'Please enter a valid password';
+      if (this.form.controls['confirmPassword'].errors) {
+        this.formErrors.confirmPassword = 'Please enter a Confirm password';
       }
 
       return false;
@@ -59,18 +77,36 @@ export class RegisterComponent {
   }
 
   registerUser() {
+    if (!this.validateForm()) {
+      return;
+    }
+    console.log(
+      this.form.controls['password'].value !==
+        this.form.controls['confirmPassword'].value
+    );
+    if (
+      this.form.controls['password'].value !==
+      this.form.controls['confirmPassword'].value
+    ) {
+      console.log('haere');
+      this.formErrors.confirmPassword =
+        'Current Password does not match new password';
+      return;
+    }
+
     const request: RegisterRequest = {
       email: this.form.controls['email'].value,
       name: this.form.controls['name'].value,
       password: this.form.controls['password'].value,
     };
-
     this.authService
       .sendVerifyCode(request)
       .pipe(take(1))
       .subscribe({
         next: (response) => {
-          this.router.navigate(['send-verify-code']);
+          this.router.navigate(['/auth/register/send-verify-code'], {
+            queryParams: { email: this.form.controls['email'].value },
+          });
         },
         error: (error) => {},
       });
