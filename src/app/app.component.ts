5cb8f62ea6e8f8e2ff64c960a657ca85c606e19e
Donald Hudson <donmuscles97@Donalds-MacBook-Pro.local>
2025-08-02 11:45:22 -0400

    add pwa code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,18 @@ import { AppService } from './app.service';
 import { AuthService } from './features/auth/services/auth.service';
 import { TopHeaderComponent } from './features/shared/components/top-header/top-header.component';
 import { BottomNavMenuComponent } from './features/shared/components/bottom-nav-menu/bottom-nav-menu.component';
+import { UpdateNotificationComponent } from './shared/components/update-notification/update-notification.component';
+import { InstallPromptComponent } from './shared/components/install-prompt/install-prompt.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TopHeaderComponent, BottomNavMenuComponent],
+  imports: [
+    RouterOutlet,
+    TopHeaderComponent,
+    BottomNavMenuComponent,
+    UpdateNotificationComponent,
+    InstallPromptComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
