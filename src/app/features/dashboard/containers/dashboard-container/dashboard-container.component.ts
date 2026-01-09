8ff4c59e4ed1d42b1bbc38aaeb3cf74e7079141d
Donald Hudson <donmuscles97@Donalds-MacBook-Pro.local>
2026-01-08 22:22:05 -0500

    add progress component to dash

--- src/app/features/dashboard/containers/dashboard-container/dashboard-container.component.ts
+++ src/app/features/dashboard/containers/dashboard-container/dashboard-container.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { TaskCardComponent } from '../../components/task-card/task-card.component';
+import { TaskProgressCardComponent } from '../../components/task-progress-card/task-progress-card.component';
 
 @Component({
   selector: 'app-dashboard-container',
-  imports: [TaskCardComponent],
+  imports: [TaskProgressCardComponent],
   templateUrl: './dashboard-container.component.html',
   styleUrl: './dashboard-container.component.scss',
 })
