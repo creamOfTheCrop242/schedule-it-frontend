a37a075a1731c06ed1d7da94bd6d649365872faf
Donald Hudson <donmuscles97@Donalds-MacBook-Pro.local>
2026-04-25 11:31:21 -0400

    add color to the ui

--- src/app/features/tasks/containers/tasks-container/tasks-container.component.ts
+++ src/app/features/tasks/containers/tasks-container/tasks-container.component.ts
@@ -5,21 +5,25 @@ import {
   computed,
   inject,
 } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { TasksService } from '../../services/tasks.service';
 
 @Component({
   selector: 'app-tasks-container',
-  imports: [RouterLink],
+  imports: [CommonModule, RouterLink],
   templateUrl: './tasks-container.component.html',
   styleUrl: './tasks-container.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TasksContainerComponent {
-  private readonly tasksService = inject(TasksService);
+  readonly tasksService = inject(TasksService);
 
   readonly allTasks = this.tasksService.allTasks;
   readonly isLoading = this.allTasks.isLoading;
+  readonly showInitialLoading = computed(
+    () => this.isLoading() && !this.allTasks.hasValue(),
+  );
   readonly hasLoadError = computed(
     () => this.allTasks.status() === ResourceStatus.Error,
   );
